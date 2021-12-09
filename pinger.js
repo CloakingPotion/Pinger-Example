1fd3c8d40fbfc81db24b3bcb2abc85637257a972
Shawn Whitaker <shawn.w8465@gmail.com>
2021-12-09 09:37:08 -0700

    Finialized

--- pinger.js
+++ pinger.js
@@ -3,7 +3,19 @@ const Pinger = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 Pinger.config = require("./config.js");
-config = Pinger.config;
+const config = Pinger.config;
+let stopped = false;
+let timestamps = [
+	{
+		localTimestamp: 123123,
+		discordTimestamp: 121234,
+		id: 32532,
+	},
+];
+
+let lastMessage;
+
+let stoppedLocalTimestamp;
 
 Pinger.on("ready", async () => {
 	Pinger.user.setActivity("Iam normal bot :kappa:");
@@ -30,6 +42,7 @@ Pinger.on("messageCreate", async (message) => {
 		return;
 	} else {
 		if (message.content.startsWith(prefix + "ping")) {
+			stopped = false;
 			let user = message.mentions.users.first();
 			if (!user)
 				return message.channel.send(
@@ -42,37 +55,89 @@ Pinger.on("messageCreate", async (message) => {
 			Pinger.user.setActivity(`Pinging: ${user.tag}`);
 			console.log(`Started pinging on: ${user.tag}!`);
 
-			interval = setInterval(async () => {
-				Pinger.channels.fetch(config.channelName).then(async (pingChannel) => {
-					if (!pingChannel) {
-						await message.guild
-							.createChannel(`${config.channelName}`)
-							.then((c) => {
-								console.log(
-									`Channel not found. Created new one.\nName: ${c.name}\nID: ${c.id}`
-								);
-								c.send(`<@${user.id}>`);
-							});
-					} else {
-						pingChannel.send(`<@${user.id}>`);
-					}
-				});
-			}, config.pingInterval);
+			startPinging(user);
 		}
 
 		if (message.content.startsWith(prefix + "stop")) {
-			await message.channel.send(
-				`**${message.author.username}**, stopping pinging!`
-			);
-			await clearInterval(interval);
-			await sleep(5000);
+			stopped = true;
+			stoppedLocalTimestamp = Date.now();
 			await message.channel.send(
 				`**${message.author.username}**, stopped pinging successfully!`
 			);
 			await Pinger.user.setActivity("Iam normal bot :kappa:");
 			console.log(`Stopped pinging by: ${message.author.tag}`);
+
+			pruneOverflow(message.channel);
 		}
 	}
 });
+
+async function startPinging(user) {
+	const pingChannel = await Pinger.channels.fetch(config.channelName);
+
+	if (!pingChannel) {
+		await message.guild.createChannel(`${config.channelName}`).then((c) => {
+			console.log(
+				`Channel not found. Created new one.\nName: ${c.name}\nID: ${c.id}`
+			);
+			c.send(`<@${user.id}>`).then(() => ping(user, pingChannel));
+		});
+	} else {
+		ping(user, pingChannel);
+	}
+
+	async function ping(user) {
+		if (stopped) {
+			return;
+		}
+
+		const pingChannel = await Pinger.channels.fetch(config.channelName);
+		if (stopped) {
+			finialize();
+			return;
+		}
+
+		let timestampObject = { localTimestamp: Date.now() };
+		console.log("Sending ping...");
+		pingChannel
+			.send(`<@${user.id}>`)
+			.then((message) => {
+				lastMessage = message;
+				if (stopped) {
+					finialize();
+					return;
+				}
+				let messageVar = message;
+				console.log(
+					`Message posted. Created at ${messageVar.createdTimestamp}`
+				);
+				timestampObject["discordTimestamp"] = messageVar.createdTimestamp;
+				timestampObject["id"] = messageVar.id;
+
+				timestamps.push(timestampObject);
+
+				ping(user);
+			})
+			.catch((error) => {
+				console.error(`An error has occured: ${error}`);
+				finialize();
+				return;
+			});
+	}
+}
+
+function finialize() {
+	lastMessage.delete();
+}
+
+function pruneOverflow() {
+	timestamps.forEach((timestampObj) => {
+		if (timestampObj.stoppedLocalTimestamp < timestampObj.discordTimestamp) {
+			timestampObject["obj"].delete().then((msg) => {
+				console.log(`Deleted overflow`);
+			});
+		}
+	});
+}
 
 Pinger.login(config.token);
